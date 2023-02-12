837a05252fd6cfc372e39ac52b3c3bcd017c506f
Krittanai <Krittanai6563@gmail.com>
2023-02-12 22:49:42 +0700

    tag and Anchor

---
+++ Tag_UWB/Tag_UWB.ino
@@ -0,0 +1,53 @@
+#include <SPI.h>
+#include "DW1000Ranging.h"
+#include "DW1000.h"
+ 
+// connection pins
+const uint8_t PIN_RST = PB12; // reset pin
+const uint8_t PIN_IRQ = PB0; // irq pin
+const uint8_t PIN_SS = PA4;   // spi select pin
+ 
+// TAG antenna delay defaults to 16384
+// leftmost two bytes below will become the "short address"
+char tag_addr[] = "7D:00:22:EA:82:60:3B:9C";
+ 
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+ 
+
+  DW1000Ranging.initCommunication(PIN_RST, PIN_SS, PIN_IRQ); //Reset, CS, IRQ pin
+ 
+  DW1000Ranging.attachNewRange(newRange);
+  DW1000Ranging.attachNewDevice(newDevice);
+  DW1000Ranging.attachInactiveDevice(inactiveDevice);
+ 
+// start as tag, do not assign random short address
+ 
+  DW1000Ranging.startAsTag(tag_addr, DW1000.MODE_LONGDATA_RANGE_LOWPOWER, false);
+}
+ 
+void loop()
+{
+  DW1000Ranging.loop();
+}
+ 
+void newRange()
+{
+  Serial.print(DW1000Ranging.getDistantDevice()->getShortAddress(), HEX);
+  Serial.print(",");
+  Serial.println(DW1000Ranging.getDistantDevice()->getRange());
+}
+ 
+void newDevice(DW1000Device *device)
+{
+  Serial.print("Device added: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
+ 
+void inactiveDevice(DW1000Device *device)
+{
+  Serial.print("delete inactive device: ");
+  Serial.println(device->getShortAddress(), HEX);
+}
