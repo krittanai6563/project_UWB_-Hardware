a90fc4308f870c433eff037863d2d20d3555cfcc
Krittanai <Krittanai6563@gmail.com>
2023-02-19 14:37:42 +0700

    update 19/2/2023

--- NRF24_esp32/NRF24_esp32.ino
+++ NRF24_esp32/NRF24_esp32.ino
@@ -2,7 +2,7 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(5, 4); // CE, CSN
+RF24 radio(4, 5); // CE, CSN
 
 const byte address[6] = "00001";
 
@@ -10,6 +10,8 @@ void setup() {
   Serial.begin(9600);
   radio.begin();
   radio.openReadingPipe(0, address);
+  radio.openReadingPipe(1, address);
+  radio.openReadingPipe(2, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
@@ -18,6 +20,17 @@ void loop() {
   if (radio.available()) {
     float text;
     radio.read(&text, sizeof(text));
+    Serial.print("ค่าจากบอร์ดที่ 1 ");
     Serial.println(text);
+
+    float text1;
+    radio.read(&text1, sizeof(text1));
+    Serial.print("ค่าจากบอร์ดที่ 2 ");
+    Serial.println(text1);
+
+    float text2;
+    radio.read(&text2, sizeof(text2));
+    Serial.print("ค่าจากบอร์ดที่ 3 ");
+    Serial.println(text2);
   }
 }
